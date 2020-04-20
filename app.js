d868fba40cf71a6e1f9e4ebc1302ff110fbcbd0a
Darshan <darshanmesta33@gmail.com>
2020-04-20 20:20:35 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,26 @@
+ const express=require('express')
+ const app=express()
+ const route=require('./router/router')
+ const mongoose=require('mongoose')
+ const config=require('./config/config')
+ const bodyParser=require('body-parser')
+ const cors=require('cors')
+
+ const PORT= Number(process.env.PORT || 4400)
+
+ mongoose.Promise=global.Promise
+ mongoose.connect(config.DB,{useNewUrlParser:true}).then(mRes=>{
+     console.log("Mongoose Connection Successful")
+ },
+ err=>{
+      console.log("Mongoose connection Failed")
+ })
+
+ app.use(bodyParser.urlencoded({extended:true}))
+ app.use(bodyParser.json())
+ app.use('/',route)
+ app.use(cors())
+
+ app.listen(PORT,()=>{
+     console.log(`The Server is up and Running at ${PORT}`)
+ })
